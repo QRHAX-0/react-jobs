67c20d11dc4ce01b5be33393386c94dc4aff31c7
yousif <javascript313@gmail.com>
2025-06-08 19:11:34 +0300

    fix: update CORS configuration to resolve frontend access issues

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(cors({
   origin:[
     'http://localhost:3000', // Frontend URL
-    'https://react-jobs-frontend-h258fbet8-qrhax-0s-projects.vercel.app/'
+    'https://react-jobs-frontend-tan.vercel.app/'
 ],
   credentials: true
 }));
